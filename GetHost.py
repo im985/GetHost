e27319f65723a2e4ae33905f35a198b0c44a77e9
Mo <61326314+im985@users.noreply.github.com>
2022-02-14 19:40:01 +0300

    Update GetHost.py

--- GetHost.py
+++ GetHost.py
@@ -4,10 +4,18 @@ print("     GetHost  \n Twitter: @im_985 \n Github: @im985 \n")
 
 
 def host():
-    URL = input("Enter URL: ")
-    byname = socket.gethostbyname(URL)
-    byaddr = socket.gethostbyaddr(URL)
-    print(f" Ip: {byname},\n Host: {byaddr}")
+    global url
+    try:
+        url = input("Enter URL: ")
+        try:
+            host = socket.gethostbyname(url)
+            hostname = socket.gethostbyaddr(host)
+            print("IP: ", host)
+            print("AdderInfo: ", hostname)
+        except:
+            print("Error: ", url, " is not a valid URL")
+    except:
+        print("Error: ", url, " is not a valid URL")
 
 
-print(host())
+host()
